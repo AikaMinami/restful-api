35263d9335bb6cabb1b813a99d102d47e86a5772
AikaMinami <aikatan.aikachan@gmail.com>
2021-04-23 23:50:21 +0700

    CRUD in RESTful API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
 });
 
 
